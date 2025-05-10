7fcf482cd8a5ff6eaa4af3e37ce5e5124e1e439c
leonardo <leonardo.studart@lansweeper.com>
2025-05-10 14:34:50 -0400

    :sparkles: add wait-for-db task

---
+++ src/infrastructure/scripts/wait-for-db.js
@@ -0,0 +1,19 @@
+const { exec } = require("node:child_process");
+function checkPostgres() {
+  exec(
+    "docker exec database-dev pg_isready -h localhost",
+    handleReturn
+  );
+
+  function handleReturn(error, stdout) {
+    if (!stdout.includes("accepting connections")) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
+  }
+}
+console.log("🟡 waiting for DB");
+checkPostgres();
