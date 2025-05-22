0579501c254f2ca123116c06540a04b8eaaf9b8a
leonardo <leonardo.studart@lansweeper.com>
2025-05-22 15:02:43 -0400

    :bug: use require

--- src/infrastructure/scripts/wait-for-db.js
+++ src/infrastructure/scripts/wait-for-db.js
@@ -1,10 +1,11 @@
-import { exec } from "node:child_process";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { exec } = require("node:child_process");
 function checkPostgres() {
   exec("docker exec database-dev pg_isready -h localhost", handleReturn);
 
   function handleReturn(error, stdout) {
     if (!stdout.includes("accepting connections")) {
-      // eslint-disable-next-line no-undef
+       
       process.stdout.write(".");
       checkPostgres();
       return;
