a9edd48dee59780148c7ceb4280d79a3bcf5f0a4
leonardo <leonardo.studart@lansweeper.com>
2025-07-08 16:18:40 -0400

    :recycle: structure tests better

--- src/application/migrations/__tests_/get.test.ts
+++ src/application/migrations/__tests_/get.test.ts
@@ -5,17 +5,19 @@ import dotenv from "dotenv";
 dotenv.config();
 const cleanDb = () => query("drop schema public cascade; create schema public");
 
-describe("tests end points /migrations", () => {
-  beforeAll(async () => {
-    await orquestrator();
-    await cleanDb();
-  });
+describe("GET to /api/migrations", () => {
+  describe("Anonymous user", () => {
+    beforeAll(async () => {
+      await orquestrator();
+      await cleanDb();
+    });
 
-  test("GET to /api/migrations returns 200", async () => {
-    const response = await fetch(`http://localhost:8080/api/migrations`);
-    expect(response.status).toBe(200);
-    const responseBody = await response.json();
-    expect(Array.isArray(responseBody)).toBe(true);
-    expect(responseBody.length).toBeGreaterThan(0);
+    test("Running pending migrations", async () => {
+      const response = await fetch(`http://localhost:8080/api/migrations`);
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
   });
 });
--- src/application/migrations/__tests_/post.test.ts
+++ src/application/migrations/__tests_/post.test.ts
@@ -5,25 +5,32 @@ import dotenv from "dotenv";
 dotenv.config();
 const cleanDb = () => query("drop schema public cascade; create schema public");
 
-describe("tests end points /migrations", () => {
-  beforeAll(async () => {
-    await orquestrator();
-    await cleanDb();
-  });
-  test("GET to /api/migrations returns 200", async () => {
-    const response1 = await fetch(`http://localhost:8080/api/migrations`, {
-      method: "POST",
-    });
-    expect(response1.status).toBe(201);
-    const response1Body = await response1.json();
-    expect(Array.isArray(response1Body)).toBe(true);
-    expect(response1Body.length).toBeGreaterThan(0);
-    const response2 = await fetch(`http://localhost:8080/api/migrations`, {
-      method: "POST",
-    });
-    expect(response2.status).toBe(200);
-    const response2Body = await response2.json();
-    expect(Array.isArray(response2Body)).toBe(true);
-    expect(response2Body.length).toBe(0);
-  });
+describe("POST to /api/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      beforeAll(async () => {
+        await orquestrator();
+        await cleanDb();
+      });
+      test("For the first time", async () => {
+        const response1 = await fetch(`http://localhost:8080/api/migrations`, {
+          method: "POST",
+        });
+        expect(response1.status).toBe(201);
+        const response1Body = await response1.json();
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBeGreaterThan(0);
+      });
+      test("For the second time", async () => {
+
+        const response2 = await fetch(`http://localhost:8080/api/migrations`, {
+          method: "POST",
+        });
+        expect(response2.status).toBe(200);
+        const response2Body = await response2.json();
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+      });
+    })
+  })
 });
